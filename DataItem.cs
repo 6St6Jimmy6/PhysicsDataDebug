e5be5e2221a2f4bc53e12298ea6953a6ff3cf28c
6St6Jimmy6 <zimbo93@msn.com>
2025-12-05 16:45:27 +0200

    DataItem in a separate file

---
+++ DataItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics_Data_Debug
+{
+    public class DataItem
+    {
+        public int Id { get; set; }
+        public int Prefix { get; set; }
+        public ulong BaseAddress { get; set; }
+        public int[] Offsets { get; set; }
+        public ulong FullAddress { get; set; }
+        public string Name { get; set; }
+        public dynamic Value { get; set; }
+        public int Unit { get; set; }
+    }
+}
--- LiveData.cs
+++ LiveData.cs
@@ -12,17 +12,6 @@ using System.Xml.Linq;
 
 namespace Physics_Data_Debug
 {
-    public class DataItem
-    {
-        public int Id { get; set; }
-        public int Prefix { get; set; }
-        public ulong BaseAddress { get; set; }
-        public int[] Offsets { get; set; }
-        public ulong FullAddress { get; set; }
-        public string Name { get; set; }
-        public dynamic Value { get; set; }
-        public int Unit { get; set; }
-    }
     public class RawTireData
     {
         public int RaceTime { get; set; }
